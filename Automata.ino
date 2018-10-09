81faa0a5fde6492eedae47cd065ff306147c83d2
email <b1017216@gmail.com>
2018-10-09 14:28:57 +0900

    照度センサーの動作確認

--- Automata.ino
+++ Automata.ino
@@ -1,9 +1,15 @@
+//参照サイト
+//照度センサーの使い方 [Arduino]
+//https://www.petitmonte.com/robot/howto_illuminance_sensor.html
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(8,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int val = digitalRead(8);
+  Serial.println(val);
 }
