aa61d9d3f196f63689f5c118cbddf853b013a2b9
email <b1017216@gmail.com>
2018-10-09 19:10:19 +0900

    LEDプログラムver1完成

--- Automata.ino
+++ Automata.ino
@@ -2,15 +2,24 @@
 //照度センサーの使い方 [Arduino]
 //https://www.petitmonte.com/robot/howto_illuminance_sensor.html
 
+int light = 8;
+int led = 9;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);//サイト参照
-  pinMode(8,INPUT);//サイト参照
-  
-}
+  pinMode(light,INPUT);//サイト参照
+  pinMode(led, OUTPUT);//LED点灯用
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int val = digitalRead(8);//サイト参照
+  int val = digitalRead(light);//サイト参照
   Serial.println(val);//サイト参照
+
+  if(val == 1){
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
 }
